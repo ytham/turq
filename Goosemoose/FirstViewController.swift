192991183533b09a2138b3e334b47a608e330a10
Yu Jiang Tham <yoojbruin@gmail.com>
2015-10-24 09:59:42 -0700

    Initial Commit

---
+++ Goosemoose/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Goosemoose
+//
+//  Created by Yu Jiang Tham on 10/24/15.
+//  Copyright © 2015 YAK. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
